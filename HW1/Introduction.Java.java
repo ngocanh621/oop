24697beef545bba48d63407eb238ba61e09e0f0a
ngocanh621 <24020034@vnu.edu.vn>
2025-09-25 12:49:19 +0700

    main

---
+++ HW1/Introduction.Java.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Họ tên:Nguyễn Ngọc Ánh\t");
+        System.out.print("Mã sinh viên:242020034\t");
+        System.out.print("Lớp:K69I-IT4-INT2204_4\t");
+        System.out.print("Username tài khoản GitHub:ngocanh621\t");
+        System.out.println("Địa chỉ email:24020034@vnu.edu.vn");
+
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
